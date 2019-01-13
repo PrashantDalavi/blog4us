aa23b8f8a25ffae60783c59e9c939ed56f5ec722
PrashantDalavi <p.d.dalavi@gmail.com>
2019-01-13 13:43:01 +0100

    generated posts and post index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home_pages/index'
   root 'home_pages#index'
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
