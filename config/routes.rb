0aa26188e7cd4e2e67e55372b7722db7404ceb86
PrashantDalavi <p.d.dalavi@gmail.com>
2019-01-14 23:47:34 +0100

    added statis contact form

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   get '/about', :to => 'pages#about'
   get '/contact', :to => 'pages#contact'
   get '/resources', :to => 'pages#resources'
+  post 'pages/thank_you'
+  get 'pages/thank_you'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
