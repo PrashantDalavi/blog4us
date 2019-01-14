4b4d4218b4aba4b19baa8db26a1fb1b8aee59e5f
PrashantDalavi <p.d.dalavi@gmail.com>
2019-01-14 02:27:35 +0100

    generated pages and added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'pages/about'
+  get 'pages/contact'
+  get 'pages/resources'
   get 'home_pages/index'
   root 'home_pages#index'
   resources :posts
   resources :categories
+
+  get '/about', :to => 'pages#about'
+  get '/contact', :to => 'pages#contact'
+  get '/resources', :to => 'pages#resources'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
